2b0eb6a99c149eb12e5a6fc3c88d836a5cfaac1e
Santiago Barbosa Nieto <60375386+SantiagoBarbosaNieto@users.noreply.github.com>
2023-08-15 18:30:40 -0500

    Set up a basic shop canvas and inventory

--- Assets/Scripts/Environment/Shop.cs
+++ Assets/Scripts/Environment/Shop.cs
@@ -4,9 +4,35 @@ using UnityEngine;
 
 public class Shop : I_Interactable
 {
+    [SerializeField]
+    GameObject ShopUI;
+    [SerializeField]
+    GameObject SlotPrefab;
+    [SerializeField]
+    GameObject ShopGrid;
     public override void Interact()
     {
-        Debug.Log("Interacting with shop");
+        ToggleShopUI();
+    }
+
+    void Awake()
+    {
+        // GameObject instance = Instantiate(SlotPrefab, transform.position, Quaternion.identity);
+        // instance.transform.SetParent(ShopGrid.transform);
+        // instance.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
+        // //If theres a need to rezise, resize the shopgrid rect vertically
+        // AddRowShopGrid();
+
+    }
+
+    private void AddRowShopGrid()
+    {
+        ShopGrid.GetComponent<RectTransform>().sizeDelta = new Vector2(ShopGrid.GetComponent<RectTransform>().sizeDelta.x,ShopGrid.GetComponent<RectTransform>().sizeDelta.y+ 60);
+    }
+
+    private void ToggleShopUI()
+    {
+        ShopUI.SetActive(!ShopUI.activeSelf);
     }
 
     public override void OnPlayerNear()
