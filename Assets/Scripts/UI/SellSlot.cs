ca56d40e0e73aa1238089219b6496440512cc834
Santiago Barbosa Nieto <60375386+SantiagoBarbosaNieto@users.noreply.github.com>
2023-08-15 23:45:04 -0500

    Sell implemented

--- Assets/Scripts/UI/SellSlot.cs
+++ Assets/Scripts/UI/SellSlot.cs
@@ -2,17 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SellSlot : MonoBehaviour
+public class SellSlot : I_Slot
 {
-    // Start is called before the first frame update
+    [Range(0,1)]
+    [SerializeField] float sellPercentage;
+    [SerializeField] InventoryManager storeInventory;
+    PlayerCoins playerCoins;
+
     void Start()
     {
+        playerCoins = FindObjectOfType<PlayerCoins>();
+    }
+    protected override void OnSomethingDropped(Transform itemDropped)
+    {
+        EquipableItem item = itemDropped.GetComponent<EquipableItem>();
+        playerCoins.AddCoins(Mathf.CeilToInt(sellPercentage*item.price));
         
+        storeInventory.GetComponent<InventoryManager>().AddItem(item);
+        item.PlayerOwned = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override bool ExtraChecksBeforeDrop(GameObject item)
     {
+        if(!item.GetComponent<EquipableItem>().PlayerOwned)
+        {   
+            Debug.Log("You don't own that! :s");
+            return false;
+        }
+        if(!storeInventory.CheckSpace())
+        {
+            Debug.Log("Store is full!");
+            return false;
+        }
         
+        return true;
     }
 }
